5b6e8694ce8d3c04c91a5560ee798c4b3fc068e2
Daniletskiy-D <slimedodoser@gmail.com>
2023-09-13 21:26:06 +0300

    Изменение 2

--- Progr.cpp
+++ Progr.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 #include <locale>
+
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
